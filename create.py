445fd60738701eeda8dffc9d73833aec51e68f43
IsthatEmma Bautista <emmabautista.2008@gmail.com>
2025-04-23 18:03:30 +0000

    re-started code

--- create.py
+++ create.py
@@ -1,6 +1,22 @@
-quiz_questions = ["What is 2+2", "What is 4+4", "What is 5+1"]
+quiz_questions = [
+    "What is 2 + 2?",
+    "What is 4 + 4?",
+    "What is 7 x 2?",
+    "What color is the sky?"
+]
 
 def quiz_question(new_question):
-    for i in range(len(quiz_questions)):
-        input_str = "Want to start with this question " + new_question  + "or this question"  + new_question  + "or this question" + quiz_questions[i] + " "
-        
+    input(new_question) ("Press enter to move on")
+
+keep_playing = True
+
+while keep_playing:
+    play_quiz = input("Would you like to play my quiz? (Y/N): ")
+    if play_quiz.upper() == "Y":
+        print("Okay, let's start!\n")
+        for question in quiz_questions:
+            quiz_question(question) 
+        print("\nThanks for playing!")
+    else:
+        print("Okay, maybe next time!")
+        keep_playing = False
