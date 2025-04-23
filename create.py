73c6106e351ee91eaded1fe91fe14d01f5c6b245
IsthatEmma Bautista <emmabautista.2008@gmail.com>
2025-04-23 22:23:48 +0000

    finished project?

--- create.py
+++ create.py
@@ -1,22 +1,21 @@
-quiz_questions = [
-    "What is 2 + 2?",
-    "What is 4 + 4?",
-    "What is 7 x 2?",
-    "What color is the sky?"
-]
+quiz_questions = ["What is 2 + 2?", "What is 4 + 4?", "What is 7 x 2?", "What color is the sky?"]
+quiz_answers = ["4", "8", "14", "Blue"]  
 
 def quiz_question(new_question):
-    input(new_question) ("Press enter to move on")
+    for i in range(len(quiz_questions)):
+        user_answer = input(new_question[i] + " ")
+        if user_answer == quiz_answers[i]:
+            print("correct")
+        else:
+            print("incorrect")
 
-keep_playing = True
+continue_playing = True
 
-while keep_playing:
-    play_quiz = input("Would you like to play my quiz? (Y/N): ")
-    if play_quiz.upper() == "Y":
-        print("Okay, let's start!\n")
-        for question in quiz_questions:
-            quiz_question(question) 
-        print("\nThanks for playing!")
-    else:
-        print("Okay, maybe next time!")
-        keep_playing = False
+while continue_playing:
+    play_quiz = input("Hello! Want to play my quiz, Yes or No: ")
+    if play_quiz == "Yes":
+        print("Awesome! Let's start")
+        quiz_question(quiz_questions)
+    else: 
+        print("Okay bye! :)")
+        continue_playing = False
